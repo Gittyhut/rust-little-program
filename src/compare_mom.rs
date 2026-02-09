cbbc61768eecd9a8b845d2882523030f5f9598aa
Gittyhut <161722363+Gittyhut@users.noreply.github.com>
2026-02-09 11:33:44 +0700

    Add age comparison program with user input

---
+++ src/compare_mom.rs
@@ -0,0 +1,25 @@
+use std::cmp::Ordering;
+
+use std::io;
+
+fn main() {
+  println!("Compare your age with my mom's age");
+
+  println!("How old are you?");
+
+  let mom_age = 39;
+
+  let mut age = String::new();
+
+  io::Stdin()
+    .read_line(&mut age)
+    .expect("Failed to read line.");
+  
+  let age: i32 = age.trim().parse().expect("How old are you?");
+
+  match age.cmp(&mom_age) {
+    Ordering::Less => println!("You're younger than my mom!"),
+    Ordering::Greater => println!("You're older than my mom!"),
+    Ordering::Equal => println!("You're the same age as my mom!"),
+  }
+}
